01b3d4338cc697066fc03a204dc73058767c288c
syoon2 <55358516+syoon2@users.noreply.github.com>
2021-11-22 19:18:43 -0400

    change Hadoop version to v3.3.1

--- get-sw.sh
+++ get-sw.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
-HADOOP_VERSION='3.3.0'
-
-#APACHE_MIRROR="http://mirror.dsrg.utoronto.ca/apache/"
-APACHE_MIRROR="http://mirror.csclub.uwaterloo.ca/apache/"
-#APACHE_MIRROR="https://www-us.apache.org/dist/"
+HADOOP_VERSION='3.3.1'
 
 HADOOP_TARFILE="hadoop-${HADOOP_VERSION}.tar.gz"
 
 mkdir -p sw
-wget -nc ${APACHE_MIRROR}${HADOOP_APACHE_PATH} -O sw/${HADOOP_TARFILE}
+wget -nc https://dlcdn.apache.org/hadoop/common/hadoop-${HADOOP_VERSION}/hadoop-${HADOOP_VERSION}.tar.gz -O sw/${HADOOP_TARFILE}
